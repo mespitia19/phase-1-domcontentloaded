bd64a7c092d95960330302b54805e500c74432ee
MaximoEspitia <max19espitia@gmail.com>
2022-03-25 15:08:41 -0500

    commit tree 0d3fc8e3f3b3635e24ca6939de15b015631d8d5f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded") , function() {
+//     console.log("The DOM has loaded");
+// }
+// console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// );
+document.addEventListener("DOMContentLoaded", (e) => {
+    let text = document.getElementById("text");
+    text.innerHTML = "This is really cool!";
+});
+
